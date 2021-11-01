fdbc0a9aa8545bb35efdbad8fb4373d934acd979
Harry Sadler <harrysadlermusic@gmail.com>
2021-11-01 21:45:51 +0100

    add basic websocket to game-server, add test webpage to hit game-server

--- GameServer/server.py
+++ GameServer/server.py
@@ -1,12 +1,23 @@
 from flask import Flask
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 
 @app.route("/")
 def hello_world():
     return "hello, world!"
 
+@socketio.on('connect')
+def test_connect():
+	print('connected')
+	emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect')
+def test_disconnect():
+	print('Client disconnected')
+
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
