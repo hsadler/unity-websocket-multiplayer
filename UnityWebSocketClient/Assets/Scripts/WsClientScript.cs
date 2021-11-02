d9460d8d85a1da302b874e950e8fed4bfed796b8
Harry Sadler <harrysadlermusic@gmail.com>
2021-11-02 23:44:05 +0100

    attempt to add a websocket client in Unity (fail)

---
+++ UnityWebSocketClient/Assets/Scripts/WsClientScript.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using WebSocketSharp;
+
+public class WsClientScript : MonoBehaviour
+{
+    WebSocket ws;
+    
+    private void Start()
+    {
+        ws = new WebSocket("ws://localhost:5000");
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            Debug.Log("Message Received from "+((WebSocket)sender).Url+", Data : "+e.Data);
+        };
+    }
+
+    private void Update()
+    {
+        if(ws == null)
+        {
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ws.Send("Hello");
+        }  
+    }
+
+}
